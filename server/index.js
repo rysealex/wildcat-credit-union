869e186733dda9013bd0e7f0d465569e1d6b6346
Alex Ryse <aryse54@gmail.com>
2025-05-22 17:42:07 -0700

    Exposed MySQL on port 3307, connected db to app, fixed init.sql.

--- server/index.js
+++ server/index.js
@@ -14,7 +14,8 @@ const db = mysql.createConnection({
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE
+  database: process.env.MYSQL_DATABASE,
+  port: 3306
 });
 
 db.connect(err => {
