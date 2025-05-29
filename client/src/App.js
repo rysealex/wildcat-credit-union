e582a38f80578cb4ee8e28d2dadd854903aa31cc
Alex Ryse <aryse54@gmail.com>
2025-05-29 14:54:43 -0700

    Fixed test.

--- client/src/App.js
+++ client/src/App.js
@@ -10,26 +10,11 @@ import TransactionHistory from "./components/transaction_history";
 function App() {
   return (
     <Router>
-<<<<<<< HEAD
-      <div>
-        <h1>WCU Frontend</h1>
-        <p>{message}</p>
-      </div>
-      <Routes>]
-        <Route path="/" element={<Homepage />} />
-
-              <Route path="/" element={<SignUp />} />
-        <Route path="/display_account_info" element={<DisplayAccountInfo />} />
-        <Route path="/" element={<SignUp />} />
-      
-
-=======
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/sign_up" element={<SignUp />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/transaction_history" element={<TransactionHistory />} />
->>>>>>> ea1d919c0b87942bef8dd13b4dc40ed2194cc125
         <Route path="/atm_locator" element={<AtmLocator />} />
         <Route path="/deposit" element={<Deposit />} />
       </Routes>
